6ae74b4682f18aa9616d13357ec62da67bd4d669
hadi-cyber <hadikusumanto2000@gmail.com>
2021-03-14 13:48:44 +0700

    UPLOADING

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function save(Request $data){
+        return view('bio', [
+            'nim' => $data['nim'],
+            'nama' => $data['nama'],
+            'kelas' => $data['kelas']
+        ]);
+    }
+}
---
+++ resources/views/add-mahasiswa.blade.php
@@ -0,0 +1,14 @@
+<h1> Input Data Mahasiswa</h1>
+<input type="button" value="kembali">
+<form action="/mahasiswa" method="POST">
+    <label for="nim">NIM : </label>
+    <input type="text" id="nim" name="nim"><br><br>
+
+    <label for="nama">Nama :</label>
+    <input type="text" id="nama" name="nama"><br><br>
+
+    <label for="kelas">Kelas : </label>
+    <input type="text" id="kelas" name="kelas"><br><br>
+
+    <input type="submit" value="Kirim">
+</form>
